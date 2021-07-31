aa4fbdd01aded2824b6431548dc56d908c65758c
salasma <67338500+salasma@users.noreply.github.com>
2021-07-30 18:55:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
+var routes = require('./routes/index');
+
 var app = express();
+
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
